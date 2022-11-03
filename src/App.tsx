b4635ce7b48c423ee01994d91bbe5d0fabee4ede
jedborseth <jedborseth@gmail.com>
2022-11-03 10:52:39 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState, useEffect, FormEvent } from "react";
+import Providers from "./components/Providers";
+function App() {
+  const [sort, setSort] = useState("movie");
+  const [search, setSearch] = useState(null);
+  useEffect(() => {
+    addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        setSearch(null);
+      }
+    });
+  }, []);
+  return (
+    <div className="w-full h-screen grid place-items-center bg-neutral-800 grid-rows-3">
+      <h1 className="text-white text-2xl decoration-blue-600 text-center">
+        Search Movies And TV Shows
+        <span className="block text-center text-xs text-gray-200 italic">
+          Searching my personal media server & TMDB and shows where to order,
+          buy, or how to watch them
+        </span>
+      </h1>
+      {search ? (
+        <Results search={search} setSearch={setSearch} sort={sort} />
+      ) : (
+        <Form search={search} setSearch={setSearch} sort={null} />
+      )}
+      {search && (
+        <div
+          className="text-white absolute top-0 right-0 w-20 h-20 border grid place-items-center cursor-pointer"
+          onClick={() => {
+            location.reload();
+          }}
+        >
+          New Search
+        </div>
+      )}
+      <div
+        className="text-white p-10 bg-neutral-600 rounded-xl hover:bg-neutral-700 hover:scale-105"
+        onClick={() => {
+          if (sort === "movie") {
+            setSort("tv");
+          } else {
+            setSort("movie");
+          }
+        }}
+      >
+        Toggle {sort} Search
+      </div>
+    </div>
+  );
+}
+
+export default App;
+type props = {
+  search: String | null;
+  setSearch: Function;
+  sort: string | null;
+};
+const Form = ({ search, setSearch }: props) => {
+  return (
+    <form
+      action="#"
+      className="w-1/2 grid place-items-center"
+      onSubmit={(e: any) => {
+        e.preventDefault();
+        if ((e.target[0] as HTMLInputElement).value) {
+          setSearch((e.target[0] as HTMLInputElement).value);
+        }
+      }}
+    >
+      <input
+        type="text"
+        autoFocus
+        className="rounded-md w-full text-center h-10"
+        placeholder="Search..."
+      />
+      <input
+        type="submit"
+        value="Go"
+        className="bg-white w-1/2 rounded-md mt-5 hover:ring-blue-600 hover:rounded-none ring-4 transition-all"
+      />
+    </form>
+  );
+};
+
+const Results = ({ search, setSearch, sort }: props) => {
+  const [providers, setProviders] = useState<object | null>(null);
+
+  const url = `http://api.themoviedb.org/3/search/${sort}/?api_key=3e377c1f356a2442895502b892470a0b&language=en-US&query=${search}&include_adult=false`;
+  const { isLoading, error, data } = useQuery(["search"], () =>
+    fetch(url).then((res) => res.json())
+  );
+
+  const getProviders = (movieId: number) => {
+    fetch(
+      `http://api.themoviedb.org/3/${sort}/${movieId}/watch/providers?api_key=3e377c1f356a2442895502b892470a0b`
+    ).then((res) => {
+      res.json().then((res) => {
+        setProviders(res);
+      });
+    });
+  };
+  return (
+    <>
+      <div className="absolute top-0 left-0"></div>
+      {providers ? (
+        <Providers providers={providers} search={data.results} sort={sort} />
+      ) : (
+        <div className="h-[33em] overflow-auto hide-scrollbar w-full">
+          {isLoading ? (
+            "Loading..."
+          ) : (
+            <>
+              {data.results.length <= 0 ? (
+                <div className="text-center text-white">No Results Found</div>
+              ) : (
+                data.results.map(
+                  (movie: { id: number; title: string; name: string }) => {
+                    return (
+                      <div
+                        key={movie.id}
+                        className="w-full h-8 cursor-pointer relative hover:text-white text-center text-neutral-600 hover:underline decoration-blue-600 underline-offset-8"
+                        onClick={() => {
+                          getProviders(movie.id);
+                        }}
+                      >
+                        {movie.title ? movie.title : movie.name}
+                      </div>
+                    );
+                  }
+                )
+              )}
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/components/Providers.tsx
@@ -0,0 +1,129 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
+
+const Providers = ({ providers, search, sort }: any) => {
+  const url = `https://watch.borsethserver.online/Items?api_key=e2edd899b3a6480595a6f179835d1f73&userId=458c3e696e054e5c907b3bbfc230a89a&parentId=${
+    sort === "movie"
+      ? "f137a2dd21bbc1b99aa5c0f6bf02a805"
+      : "a656b907eb3a73532e40e44b968d0225"
+  }`;
+  const { isLoading, error, data } = useQuery(["jellyfin"], () =>
+    fetch(url).then((res) => res.json())
+  );
+
+  const [movie, setMovie] = useState<{
+    title: string;
+    id: number;
+    release_date: string;
+    first_air_date: string;
+  } | null>(null);
+  const [found, setFound] = useState(false);
+
+  useEffect(() => {
+    setFound(false);
+    search.map((movie: { title: string; id: number; release_date: string }) => {
+      if (movie.id === providers.id) {
+        setMovie(movie);
+      }
+    });
+    if (!isLoading && movie) {
+      console.log(data);
+      data.Items.forEach((item: { UserData: { Key: number } }) => {
+        if (movie.id === Number(item.UserData.Key)) {
+          console.log("found in jellyfin");
+          setFound(true);
+        } else {
+          console.log("no");
+        }
+      });
+    }
+  }, [data]);
+
+  return (
+    <div className="text-white h-full w-1/2 grid place-items-center">
+      {movie && (
+        <>
+          <h1 className="text-2xl">{movie.title}</h1>
+          {console.log(movie)}
+          <span>
+            "
+            {movie.release_date
+              ? new Date(movie.release_date).getFullYear()
+              : new Date(movie.first_air_date).getFullYear()}
+            "
+          </span>
+          <div>
+            {found && (
+              <h2 className="text-red-500 text-3xl bg-white rounded p-1">
+                Movie is Available on borsethserver.online
+              </h2>
+            )}
+            {providers.results.CA ? (
+              <>
+                {providers.results.CA.flatrate && (
+                  <>
+                    <h2 className="text-xl">Avaliable at</h2>
+                    <ul className="mb-10">
+                      {providers.results.CA.flatrate.map(
+                        (provider: {
+                          provider_id: number;
+                          provider_name: string;
+                          logo_path: string;
+                        }) => {
+                          return (
+                            <li
+                              key={provider.provider_id}
+                              className="flex w-full justify-between"
+                            >
+                              {provider.provider_name}
+                              <img
+                                src={`https://image.tmdb.org/t/p/w500/${provider.logo_path}`}
+                                alt=""
+                                className="w-10 rounded-full"
+                              />
+                            </li>
+                          );
+                        }
+                      )}
+                    </ul>
+                  </>
+                )}
+                {providers.results.CA.buy && (
+                  <>
+                    <h2 className="text-xl">Avaliable for Purchase</h2>
+                    <ul>
+                      {providers.results.CA.buy.map(
+                        (provider: {
+                          provider_id: number;
+                          provider_name: string;
+                        }) => {
+                          return (
+                            <li
+                              key={provider.provider_id}
+                              className="flex w-full justify-between"
+                            >
+                              {provider.provider_name}
+                              <img
+                                src={`https://image.tmdb.org/t/p/w500/${provider.logo_path}`}
+                                alt=""
+                                className="w-10 rounded-full"
+                              />
+                            </li>
+                          );
+                        }
+                      )}
+                    </ul>
+                  </>
+                )}
+              </>
+            ) : (
+              <span>Not Available in Canada</span>
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Providers;
